fb6f39e5ed9dbcc19da1d48a04e7f615f338c398
aasmolkin <aasmolkin@sberbank.ru>
2024-12-30 09:10:11 +0300

    add aliases

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@block': path.resolve(__dirname, './src/components/block'),
+      '@layout': path.resolve(__dirname, './src/components/layout'),
+      '@page': path.resolve(__dirname, './src/components/page'),
+      '@ui': path.resolve(__dirname, './src/components/ui'),
+      '@app': path.resolve(__dirname, './src/components/app'),
+    },
+  },
+};
